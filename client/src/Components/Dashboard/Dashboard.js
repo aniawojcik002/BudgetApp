c597a051c376d3cb09a7c30bb1cc9f33022e4f49
ania.wojcik002 <anna.wojcik002@gmail.com>
2022-10-27 21:39:01 +0200

    Add wrapper do Dashboard styles

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,18 +1,27 @@
-import Header from "../Header/Header"
-import {styles} from "./Dashboard.module.css";
-
-
+import Header from "../Header/Header";
+import styles from "./Dashboard.module.css";
 
 export const Dashboard = () => {
   return (
     <>
-    <div className="dashboardWrapper">
-    <Header />
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-    </div>
-
+      <div className={styles.dashboardPageWrapper}>
+        <Header />
+        <div>
+          <h1>Dashboard</h1>
+          <div className={styles.transactionsHeader}>
+            <div className={styles.searchBox}>
+              <input type="text"></input>
+            </div>
+            <div className={styles.calendarBox}>
+              <input type="date"></input>
+            </div>
+          </div>
+          <div className={styles.buttons}>
+            <button>Add spending</button>
+            <button>Delete spending</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
